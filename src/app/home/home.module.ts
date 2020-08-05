e730ead97155af2ba0a76db0e4a88c55dceca3bf
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-08-04 22:33:55 -0300

    criando a tela calendario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NgCalendarModule } from 'ionic2-calendar';
+import { CalendarioModalPageModule } from '../pages/calendario-modal/calendario-modal.module';
 
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe);
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgCalendarModule,
+    CalendarioModalPageModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-DE' }
+  ]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  eventSource = [];
+  viewTitle: string;
+
+  calendar = {
+    mode: 'month',
+    currentDate: new Date()
+  };
+
+  selectedDate: Date;
+
+  @ViewChild(CalendarComponent) myCal: CalendarComponent;
 
   constructor() {}
+
+  ngOnInit(){}
+
+  next() {
+    this.myCal.slideNext();
+  }
+
+  back() {
+    this.myCal.slidePrev();
+  }
+
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
 
 }
